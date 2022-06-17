7be0f29f4d31b8ab785131409e42ca54d540a9e7
Amy Dyson <amy.dyson@mac.com>
2022-06-16 20:40:31 -0700

    add add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {FormControl} from '@angular/forms';
+import { MatSelectChange } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,21 @@ import {FormControl} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'visualize-git';
-  commands = [
+  public title = 'visualize-git';
+  public commands = [
     {name: 'add'},
     {name: 'clone'},
     {name: 'init'}
-  ]
+  ];
+  public chosenCommand: string;
+
+  @Output() selectionChange: EventEmitter< MatSelectChange >
 
   ngOnInit(): void {
-    
+    this.chosenCommand = '';
+  }
+
+  selectCommand(command){
+    this.chosenCommand = command;
   }
 }
